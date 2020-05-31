5588c9c849c499ef0302f27eaa68d0051f32d3de
D1STR4CT <50370573+D1STR4CT@users.noreply.github.com>
2020-05-31 13:18:55 +0200

    knock-knock

---
+++ knock-knock.py
@@ -0,0 +1,15 @@
+# knock knock
+ 
+from time import sleep 
+
+
+print("Knock Knock\n")
+sleep(1)
+print("Who's there?\n")
+sleep(1)
+print("Fuck\n")
+sleep(1)
+print("Fuck Who?\n")
+sleep(1)
+print("Fuck you")
+input()
